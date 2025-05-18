869696e2a388ef3ecdfff45b3f8fb5bae8093a72
Ari <aronf11121@gmail.com>
2025-05-18 18:18:28 -0400

    Initial commit of Pomodoro app

---
+++ main.py
@@ -0,0 +1,173 @@
+from flask import Flask, render_template, flash, request, redirect, url_for
+from random import choice, randint, shuffle
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import Integer, String, ForeignKey, LargeBinary
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from cryptography.fernet import Fernet
+from dotenv import load_dotenv
+import json
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("FLASK_KEY")
+
+key = os.getenv("ENCRYPTION_KEY").encode()
+cipher = Fernet(key)
+
+class Base(DeclarativeBase):
+    pass
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///passwords.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.get(Users, user_id)
+
+class Users(db.Model, UserMixin):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
+    name: Mapped[str] = mapped_column(String(1000))
+    passwords: Mapped[list["Passwords"]] = relationship(back_populates="user")
+
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+class Passwords(db.Model):
+    __tablename__ = "passwords"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    site: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    username: Mapped[str] = mapped_column(String, nullable=False)
+    password: Mapped[str] = mapped_column(LargeBinary, nullable=False)
+    user: Mapped["Users"] = relationship(back_populates="passwords")
+
+with app.app_context():
+    db.create_all()
+
+@app.route("/")
+def welcome():
+    return render_template('welcome.html')
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.form:
+        form = request.form
+        user = db.session.execute(db.select(Users).where(Users.email == form['email'])).scalar()
+        if user:
+            flash('The email you entered is already registered. Try logging in instead')
+            return redirect(url_for('login'))
+        else:
+            password = generate_password_hash(form['password'], 'pbkdf2:sha256', 6)
+            new_user = Users(
+                name = form['name'],
+                email = form['email'],
+                password = password
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user)
+            return redirect(url_for('home'))
+    return render_template('register.html')
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.form:
+        form = request.form
+        user = db.session.execute(db.select(Users).where(Users.email == form['email'])).scalar()
+        if not user:
+            flash('The email you entered is not registered', 'error')
+        elif not check_password_hash(user.password, form['password']):
+            flash('The password you entered does not match', 'error')
+        else:
+            login_user(user)
+            return redirect(url_for('home'))
+    return render_template('login.html')
+
+@app.route("/home")
+@login_required
+def home():
+    info = db.session.execute(db.select(Passwords).where(Passwords.user_id == current_user.id)).scalars().all()
+    password_list = [cipher.decrypt(p.password).decode() for p in info]
+    data = list(zip(info, password_list))
+    return render_template('index.html', data=data)
+
+@app.route("/add-password", methods=["GET", "POST"])
+@login_required
+def add_password():
+    if request.method == "POST":
+        form = request.form
+        exists = db.session.execute(db.select(Passwords).where(Passwords.site == form['site'])).scalar()
+        if "generate" in form:
+            password = pass_generator()
+            return render_template('add.html', password=password)
+        elif exists:
+            flash('The site/app you entered is already registered.')
+        else:
+            new_entry = Passwords(
+                site=form['site'],
+                username=form['username'],
+                password=cipher.encrypt(form['password'].encode()),
+                user_id=current_user.id
+            )
+            db.session.add(new_entry)
+            db.session.commit()
+            return redirect(url_for('home'))
+    return render_template('add.html')
+
+# @app.route("/edit-entry/<int:entry_id>", methods=["GET", "POST"])
+# def edit_entry(entry_id):
+#     post = db.get_or_404(Passwords, entry_id)
+#
+#     if edit_form.validate_on_submit():
+#         post.title = edit_form.title.data
+#         post.subtitle = edit_form.subtitle.data
+#         post.img_url = edit_form.img_url.data
+#         post.author = current_user
+#         post.body = edit_form.body.data
+#         db.session.commit()
+#         return redirect(url_for("show_post", post_id=post.id))
+#     return render_template("make-post.html", form=edit_form, is_edit=True)
+
+@app.route("/delete/<int:entry_id>")
+def delete_entry(entry_id):
+    entry_to_delete = db.get_or_404(Passwords, entry_id)
+    db.session.delete(entry_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('welcome'))
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+def pass_generator():
+    password_list = []
+    password_list += [choice(letters) for _ in range(randint(8, 10))]
+    password_list += [choice(numbers) for _ in range(randint(2, 4))]
+    password_list += [choice(symbols) for _ in range(randint(2, 4))]
+    shuffle(password_list)
+    return "".join(password_list)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
